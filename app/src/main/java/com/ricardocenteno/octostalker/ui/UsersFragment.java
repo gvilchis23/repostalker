4f387d712188bd3faf918f1491deb8b4104209bb
Ricardo Centeno <ricardo.centeno@mobintum.com>
2018-09-04 10:46:05 -0500

    create a cardview for users recyclerview

---
+++ app/src/main/java/com/ricardocenteno/octostalker/ui/UsersFragment.java
@@ -0,0 +1,35 @@
+package com.ricardocenteno.octostalker.ui;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ricardocenteno.octostalker.R;
+
+public class UsersFragment extends Fragment {
+
+    private UsersViewModel mViewModel;
+
+    public static UsersFragment newInstance() {
+        return new UsersFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.users_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UsersViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ricardocenteno/octostalker/ui/UsersViewModel.java
@@ -0,0 +1,7 @@
+package com.ricardocenteno.octostalker.ui;
+
+import android.arch.lifecycle.ViewModel;
+
+public class UsersViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
